4e599c00b4a44edc717f1171f89373ee4f67bef8
macevedoPhonix <macevedo@phonix.network>
2025-12-10 10:58:35 -0500

    fix: changes requested done

--- src/lib/services/api/rolesApi/rolesApi.ts
+++ src/lib/services/api/rolesApi/rolesApi.ts
@@ -1,5 +1,5 @@
 import { api } from '../api';
-import { RolesResponse } from './interface/roles.interface';
+import { RolesResponse } from './interface';
 
 export const rolesApi = api.injectEndpoints({
   endpoints: (builder) => ({
