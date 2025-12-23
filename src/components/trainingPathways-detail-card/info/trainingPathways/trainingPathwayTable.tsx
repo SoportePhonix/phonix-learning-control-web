b8d0138a04fdb8f0f9f400917fcf72b55bb5d1ee
SofiPhonix <smurillo@phonix.network>
2025-12-22 22:30:09 -0500

    feat: avance

--- src/components/trainingPathways-detail-card/info/trainingPathways/trainingPathwayTable.tsx
+++ src/components/trainingPathways-detail-card/info/trainingPathways/trainingPathwayTable.tsx
@@ -1,9 +1,7 @@
-'use client';
+/*'use client';
 
 import { DataTable } from '@/components/ui/data-table';
-import { getTrainingRoutesColumns } from '@/hooks/students/info/trainingRoute/trainingRoutes.columns';
-import { trainingRoutesMock } from '@/hooks/students/info/trainingRoute/trainingRoutes.mock';
-import { TrainingRoute } from '@/hooks/students/info/trainingRoute/trainingRoutes.types';
+
 
 export function TrainingRoutesTable({
   onSelect,
@@ -21,7 +19,7 @@ export function TrainingRoutesTable({
     
     w-[748px] h-[341px] flex flex-col"
     >
-      <DataTable columns={getTrainingRoutesColumns(onSelect, selectedRouteId)} data={trainingRoutesMock} />
+      <DataTable columns={} data={} />
     </div>
   );
-}
+}*/
