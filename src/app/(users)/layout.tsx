f38d5a7d0aea9aefc5089cf386b8e1860cbd5c18
BricoPhonix <brico@phonix.network>
2025-12-26 11:17:20 -0500

    feat: avance 1

--- src/app/(users)/layout.tsx
+++ src/app/(users)/layout.tsx
@@ -29,7 +29,7 @@ export default async function UserLayout({ children }: { children: React.ReactNo
               <SidebarProvider defaultOpen={defaultOpen}>
                 <RtkRequestsProvider>
                   <div className="flex h-screen w-screen">
-                    <SidebarTrigger className="group-data-[collapsible=offcanvas]:fixed fixed text-var--primary-50" />
+                    <SidebarTrigger className="group-data-[collapsible=offcanvas]:fixed fixed text-[#13404d]" />
                     <AppSidebar />
                     <main className="flex-1 md:px-8 overflow-y-scroll">{children}</main>
                     <Toaster richColors position="top-right" />
