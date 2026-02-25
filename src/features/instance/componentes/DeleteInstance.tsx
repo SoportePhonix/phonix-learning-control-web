2898a71daf5a0cffe86629e5f5f0a5fb534c1c79
SofiPhonix <smurillo@phonix.network>
2026-02-25 13:14:53 -0500

    feat: use case for deleting a instance

---
+++ src/features/instance/componentes/DeleteInstance.tsx
@@ -0,0 +1,22 @@
+import { AlertConfirmDialogDestructive } from '@/components/AlertConfirmDialogDestructive';
+import { useTranslation } from '@/i18n';
+
+import { useDeleteInstance } from '../hooks/useDeleteInstance';
+
+export const DeleteInstance = ({ instanceNit }: { instanceNit: string }) => {
+  const { t } = useTranslation();
+  const { deleteInstance, isLoading } = useDeleteInstance();
+
+  return (
+    <AlertConfirmDialogDestructive
+      tooltipText={t('d.deleteInstance')}
+      title={t('d.deleteInstance')}
+      description={t('a.areYouSureYouWantToDeleteThisInstance')}
+      onConfirm={async () => await deleteInstance(instanceNit)}
+      confirmText={t('d.delete')}
+      cancelText={t('c.cancel')}
+      variant="destructive"
+      isLoading={isLoading}
+    />
+  );
+};
